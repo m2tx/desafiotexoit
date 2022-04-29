250b7545a5281d92d253f254b2387f742632646b
Mathias Roberto Back <mathiasrback@gmail.com>
2022-04-29 19:44:37 -0300

    :tada: Initial Commit

---
+++ src/main/java/br/com/github/m2tx/desafiotexoit/DesafiotexoitApplication.java
@@ -0,0 +1,13 @@
+package br.com.github.m2tx.desafiotexoit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafiotexoitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafiotexoitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/github/m2tx/desafiotexoit/DesafiotexoitApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.github.m2tx.desafiotexoit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafiotexoitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
