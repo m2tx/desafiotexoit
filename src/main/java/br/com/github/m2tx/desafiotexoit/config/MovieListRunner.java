aee8fec909e7bc3981d251f9f4ae902baa828bc6
Mathias Roberto Back <mathiasrback@gmail.com>
2022-04-30 23:58:41 -0300

    :sparkles: Add param movielist

--- src/main/java/br/com/github/m2tx/desafiotexoit/config/MovieListRunner.java
+++ src/main/java/br/com/github/m2tx/desafiotexoit/config/MovieListRunner.java
@@ -3,10 +3,12 @@ package br.com.github.m2tx.desafiotexoit.config;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.Reader;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.commons.collections.CollectionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -32,29 +34,38 @@ public class MovieListRunner implements ApplicationRunner {
 
 	@Override
 	public void run(ApplicationArguments args) throws Exception {
-		ResourcePatternResolver patternResolver = new PathMatchingResourcePatternResolver();
-		Resource resource = patternResolver.getResource("classpath:movielist.csv");
-		try(Reader reader = new BufferedReader(new InputStreamReader(resource.getInputStream()))){
-			Map<String, String> mapping = new HashMap<String, String>();
-			mapping.put("title", "title");
-			mapping.put("year", "year");
-			mapping.put("producers", "producers");
-			mapping.put("studios", "studios");
-			mapping.put("winner", "winner");
-			HeaderColumnNameTranslateMappingStrategy<Movie> mappingStrategy = new HeaderColumnNameTranslateMappingStrategy<Movie>();
-			mappingStrategy.setType(Movie.class);
-			mappingStrategy.setColumnMapping(mapping);
+		List<String> files = args.getOptionValues("movielist");
+		if(CollectionUtils.isEmpty(files)) {
+			files = Arrays.asList("classpath:movielist.csv");
+		}
+		for(String file:files) {
+			if(!file.contains("classpath:") && !file.contains("file:")) {
+				file = "file:"+file;
+			}
+			ResourcePatternResolver patternResolver = new PathMatchingResourcePatternResolver();
+			Resource resource = patternResolver.getResource(file);
+			try(Reader reader = new BufferedReader(new InputStreamReader(resource.getInputStream()))){
+				Map<String, String> mapping = new HashMap<String, String>();
+				mapping.put("title", "title");
+				mapping.put("year", "year");
+				mapping.put("producers", "producers");
+				mapping.put("studios", "studios");
+				mapping.put("winner", "winner");
+				HeaderColumnNameTranslateMappingStrategy<Movie> mappingStrategy = new HeaderColumnNameTranslateMappingStrategy<Movie>();
+				mappingStrategy.setType(Movie.class);
+				mappingStrategy.setColumnMapping(mapping);
 
 
-			CsvToBean<Movie> csvToBean = new CsvToBeanBuilder<Movie>(reader)
-					.withType(Movie.class)
-					.withSeparator(';')
-					.withIgnoreLeadingWhiteSpace(true)
-					.withMappingStrategy(mappingStrategy)
-					.build();
+				CsvToBean<Movie> csvToBean = new CsvToBeanBuilder<Movie>(reader)
+						.withType(Movie.class)
+						.withSeparator(';')
+						.withIgnoreLeadingWhiteSpace(true)
+						.withMappingStrategy(mappingStrategy)
+						.build();
 
-			List<Movie> movies = csvToBean.parse();
-			movies.forEach(movie -> service.save(movie));
+				List<Movie> movies = csvToBean.parse();
+				movies.forEach(movie -> service.save(movie));
+			}
 		}
 	}
 
